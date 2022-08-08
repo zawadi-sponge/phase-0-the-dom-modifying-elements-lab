8dd14cc7b72d91b3b6e7ddd78110517dd2070808
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-08 22:03:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remover();
+const newHeader = document.getElementById('victory');
+newHeader.innerHTML = 'Zawadi is the champion'
